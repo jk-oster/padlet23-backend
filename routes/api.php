723c6800f74ea644b5afb5c69221a365d5fe7520
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-25 20:32:40 +0100

    [TASK] added invitation and rating routes

--- routes/api.php
+++ routes/api.php
@@ -24,24 +24,38 @@ Route::get('', static function (Request $request) {
     return response()->json(['code' => 'seas']);
 });
 
+// Auth actions
 Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/logout', [AuthController::class, 'logout']);
 
 // methods which need authentication - JWT Token
 Route::group(['middleware' => ['api', 'auth.jwt']], static function () {
+    // Auth actions
     Route::get('auth/refresh', [AuthController::class, 'refresh']);
     Route::get('auth/me', [AuthController::class, 'me']);
-//    Route::get('auth/register', [AuthController::class, 'register']);
+    Route::get('auth/register', [AuthController::class, 'register']);
+
+    // User actions
+    Route::get('search/user/{searchTerm}', [AuthController::class, 'search']);
+
+    // Special get Padlets
     Route::get('shared/padlet', [PadletController::class, 'getSharedPadlets']);
     Route::get('user/padlet/{id}', [PadletController::class, 'getPadletUsersByPadletId']);
     Route::get('private/padlet', [PadletController::class, 'getPrivatePadlets']);
+
+    // Padlet actions
     Route::post('padlet/{id}/share', [PadletController::class, 'sharePadlet']);
     Route::put('padlet/{id}/toggle', [PadletController::class, 'toggle']);
-    Route::get('search/user/{searchTerm}', [AuthController::class, 'search']);
+
+    // Padlet User actions
+    Route::get('padlet-user', [PadletController::class, 'getPadletUsers']);
+    Route::put('padlet-user/{id}', [PadletController::class, 'acceptPadlet']);
+    Route::delete('padlet-user/{id}', [PadletController::class, 'declinePadlet']);
 });
+// Unsplash image search
 Route::get('search/image/{searchTerm}', [UnsplashController::class, 'search']);
 
-Route::post('auth/logout', [AuthController::class, 'logout']);
-
+// Padlet actions
 Route::get('padlet', [PadletController::class, 'index']);
 Route::get('padlet/{id}', [PadletController::class, 'show']);
 Route::post('padlet', [PadletController::class, 'store']);
@@ -49,6 +63,7 @@ Route::put('padlet/{id}', [PadletController::class, 'update']);
 Route::get('search/padlet/{search}', [PadletController::class, 'search']);
 Route::delete('padlet/{id}', [PadletController::class, 'destroy']);
 
+// Post actions
 Route::get('padlet/{id}/post', [PostController::class, 'getPostsByPadletId']);
 Route::get('post/{id}', [PostController::class, 'show']);
 Route::post('post', [PostController::class, 'store']);
@@ -56,12 +71,14 @@ Route::put('post/{id}', [PostController::class, 'update']);
 Route::delete('post/{id}', [PostController::class, 'destroy']);
 Route::get('search/padlet/{padletId}/post/{search}', [PostController::class, 'search']);
 
+// Comment actions
 Route::get('post/{id}/comment', [CommentController::class, 'getCommentsByPostId']);
 Route::post('comment', [CommentController::class, 'store']);
 Route::put('comment/{id}', [CommentController::class, 'update']);
 Route::delete('comment/{id}', [CommentController::class, 'destroy']);
 Route::get('search/post/{postId}/comment/{search}', [CommentController::class, 'search']);
 
+// Rating actions
 Route::get('post/{id}/rating', [RatingController::class, 'getRatingsByPostId']);
 Route::post('rating', [RatingController::class, 'store']);
 Route::put('rating/{postId}', [RatingController::class, 'update']);
